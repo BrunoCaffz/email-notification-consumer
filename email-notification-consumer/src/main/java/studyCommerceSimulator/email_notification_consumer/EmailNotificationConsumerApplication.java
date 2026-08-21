cfe287e9f1b432858d1e07597aee818442d447fa
BrunoCaffz <brunogkaffe@gmail.com>
2026-08-20 23:12:57 -0300

    dist: first commit updating application.properties

---
+++ email-notification-consumer/src/main/java/studyCommerceSimulator/email_notification_consumer/EmailNotificationConsumerApplication.java
@@ -0,0 +1,13 @@
+package studyCommerceSimulator.email_notification_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailNotificationConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailNotificationConsumerApplication.class, args);
+	}
+
+}
---
+++ email-notification-consumer/src/test/java/studyCommerceSimulator/email_notification_consumer/EmailNotificationConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package studyCommerceSimulator.email_notification_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
